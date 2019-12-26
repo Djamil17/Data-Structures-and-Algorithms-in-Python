61a342dc4bc026d480cecb2f11860c00110a81e5
Djamil Lakhdar Hamina <43583479+Djamil17@users.noreply.github.com>
2019-12-26 17:54:49 -0500

    Update join_algorithms.py

--- join_algorithms.py
+++ join_algorithms.py
@@ -1,32 +1,50 @@
+
+import random
+
 ## function
 class Relation:
 
     def __init__(self, n, m, init=True):
         self.n = n
         self.m = m
+        self.size= n * m
         if init:
             self.relation = [[random.randint(0,5)] * n for x in range(m)]
         else:
             self.relation= [ ]
 
+    def setTuple(self,idx, item):
+        self.relation[idx] = item
+
+    def setAtt(self, *arg):
+      if len(list(arg)) == self.n:
+        return self.relation.insert(0, list(arg))
+      else:
+        raise ValueError('Need to make attribute list same size as n...')
+
+    def __setitem__(self,idx,item):
+        self.relation[idx]=item
+
     def __getitem__(self, idx):
         return self.relation[idx]
 
-    def __repr__(self):
-        s = str(self.relation)
-        return s
+    def __str__(self):
+        return (str(self.relation))
 
-    def setTuple(self,idx, item):
-      self.relation[idx] = item
+def nested_loop_join(left_relation, right_relation):
+    for ltuple in l[1:]:
+        for rtuple in r[1:]:
+            if ltuple == rtuple:
+                yield ltuple + rtuple
 
-    def setAtt(self, *arg):
-      return self.relation.insert(0, list(arg))
+#
+r= Relation(5,5)
+l= Relation(5,5)
+l.setAtt('foo','bar','tar', 'grault','baaz')
+r.setAtt('foo','bar','tar', 'grault','baaz')
+print(r)
+print(l)
+n=nested_loop_join(l,r)
+print(list(n))
 
-    def size(self, n, m):
-        return n * m
 
-def nested_loop_join(left_relation, right_relation, attribute_a):
-    for ltuple in left_relation[1:]:
-      for rtuple in right_relation[1:]:
-        if ltuple is attribute_a and rtuple is attribute_a and ltuple==rtuple:
-           return '<' + ltuple + ',' + rtuple + '>'
