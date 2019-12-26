7d26eb7cc625b207e950e69f59b49a26acc7873d
Djamil Lakhdar Hamina <43583479+Djamil17@users.noreply.github.com>
2019-12-26 12:48:11 -0500

    Update searching_and_sorting_algorithms.py
    hashtable

--- searching_and_sorting_algorithms.py
+++ searching_and_sorting_algorithms.py
@@ -35,6 +35,60 @@ def binarySearch(alist, item):
         else midpoint < item: 
             binarySearch(alist[midpoint+1:],item)
 
+## hash table
+## hashtable/map adt 
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        if not isinstance(self.size,numbers.Integral):
+            raise TypeError('Size of slots must be a prime number')
+        self.slots= [None] * self.size
+        self.data= [None] * self.size 
+
+    def __str__(self):
+        print(self.size)
+        print(self.slots)
+        print(self.data)
+    
+    def put(self, key, data):
+        hashvalue=hashfunction(key,len(self.slots))
+
+        if self.slots[hashvalue] == None :
+            self.slots[hashhvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue]=data
+            else:
+                nextslot=rehash(hashvalue, len(self.slots))
+                while nextslot != None and self.slots[nextslot] != key: 
+                    nextslot=rehash(hashvalue, len(self.slots))
+
+                if self.slots[nextslot] == None: 
+                    self.slots[nextslot]=key
+                    self.data[nextslot]=data
+
+    def get(self, key):
+        stop=False
+        found=False
+        position=startslot
+        while self.slots[position] !=None and not found and not stop:
+            if self.slots[position] == key:
+                stop=True
+                found=True 
+                return self.data[key]
+
+    def hashfunction(self,key,size):
+        return key%size
+    
+    def rehash(self,oldhash,size):
+        return (oldhash+1)%size
+        
+    def __get__item(self,key):
+        return self.get(key)
+
+    def __set_item(self, key ,data):
+        return self.put(key,data)
 
 ## bubble sort 
 start_time = time.time()
