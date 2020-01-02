c82fddc215ee9338cefd7fea6df9ba8b563d1567
Djamil Lakhdar Hamina <43583479+Djamil17@users.noreply.github.com>
2020-01-02 15:34:09 -0500

    Update searching_and_sorting_algorithms.py

--- searching_and_sorting_algorithms.py
+++ searching_and_sorting_algorithms.py
@@ -198,10 +198,45 @@ def mergesort(alist):
             k=k+1
     
     return f'The merged list is : {alist}'
-    
+   
 ##endregion: *************
+## region: quicksort
 
+def quickSort(alist,first, last):
+    quickSortHelper(alist, 0 ,len(alist)-1)
+    return alist
+
+def quickSortHelper(alist, first,last):
+    if first<last:
+
+        splitpoint=partition(alist, first, last)
+
+        quickSortHelper(alist, first, splitpoint-1)
+        quickSortHelper(alist,splitpoint+1, last)
+
+def partition(alist, first, last):
+    pivotvalue=alist[first]
+    leftmark=first + 1
+    rightmark=last
+
+    done=False
+
+    while not done :
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark +=1
+        while rightmark >= leftmark and alist[rightmark] >= pivotvalue:
+            rightmark -=1
+        if rightmark < leftmark:
+            done=True
+        else:
+            alist[leftmark],alist[rightmark]=alist[rightmark],alist[leftmark]
+
+    alist[first] ,alist[rightmark]= alist[rightmark], alist[first]
+
+    return rightmark
+   
+##endregion: ***********
+   
 if __name__ == '__main__': 
-main(): 
 
 ## endregion 
