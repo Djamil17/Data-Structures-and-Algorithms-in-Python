030b69b2f787b44b72000edbd64c448f0d2caa2f
Djamil Lakhdar Hamina <43583479+Djamil17@users.noreply.github.com>
2020-02-04 10:42:05 -0500

    Update Graphs.py
    Created a vertex class for an adjacency list implementation

--- Graphs.py
+++ Graphs.py
@@ -1,1 +1,27 @@
+"""
 
+"""
+
+## Adjacency Matrix for Fully Connected and "Small" Graph using nested list as representation
+
+## Adjacency List for Graph using Nested Dictionary as Internal Representation 
+
+Class Vertex:
+  def __init__(self,key):
+    self.id=key
+    self.connectedTo={}
+    
+  def addNeighbor(self, nbr,weight):
+    self.connectedTo[nbr]=weight
+  
+  def getConnections(self):
+    return self.connectedTo.keys()
+  
+  def getID(self):
+    return self.id
+  
+  def getWeight(self,key):
+    return self.connectedTo[key]
+  
+  def __str__(self):
+    return str(self.id) + 'is connected to:' + str(self.connectedTo.values.iters())
