7b4895e9be968e0d2f1dc6c6ea4b936c7751400b
dl2774@columbia.edu <dl2774@columbia.edu>
2020-02-10 21:58:37 -0500

    Graphs

--- graphs.py
+++ graphs.py
@@ -4,6 +4,7 @@ Author: Djamil Lakhdar-Hamina
 Date: 02/05/2020
 """
 
+from linear_data_structures import Queue
 ## Adjacency Matrix for Fully Connected and "Small" Graph using nested list as representation
 
 ## Adjacency List for Graph using Nested Dictionary as Internal Representation
@@ -11,12 +12,33 @@ class Vertex:
     def __init__(self,key):
         self.id=key
         self.isconnectedTo={}
+        self.color=None
+        self.distance=None
+        self.pred=[]
+
+    def setPred(self, pre):
+        self.pred=pre
+
+    def setDistance(self, n):
+        self.distance=n
+
+    def setColor(self,flavor):
+        self.color=flavor
 
     def addNeighbor(self, nbr,weight):
         self.isconnectedTo[nbr]=weight
 
+    def isConnectedto(self):
+        return self.isconnectedTo
+
+    def getPred(self):
+        return self.Pred
+
     def getKey(self):
         return self.id
+
+    def getColor(self):
+        return self.color
 
     def __str__(self):
         return str(self.id) +  ' ' + 'is connected to'  + '' + ':' + str([i for i in self.isconnectedTo.keys()])
@@ -56,11 +78,31 @@ class Graph:
     def getVertexes(self):
         return self.vertList.keys()
 
+
     def __contains__(self, item):
         return item in self.vertList
 
     def __iter__(self):
         return iter(self.vertList.values())
+
+def bfs( ):
+    start=Vertex()
+    VertQueue=Queue()
+    start.setPred(None)
+    start.setColor(None)
+    start.setDistance(0)
+    VertQueue.enqueue(start)
+    while VertQueue.size() > 0 :
+        currentNode=VertQueue.dequeue()
+        for nbr in start.isconnectedTo():
+            if nbr.getColor()== 'white':
+                nbr.setColor('grey')
+                nbr.setDistance(currentNode.getDistance() + 1)
+                VertQueue.enqueue(nbr)
+                currentNode.setPred(nbr)
+        currentNode.setColor('black')
+
+
 
 
 if __name__=='__main__':
