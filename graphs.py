fea3c4fcd15aa04cafa830ecce9e4c892bc3a625
dl2774@columbia.edu <dl2774@columbia.edu>
2020-02-15 13:46:13 -0500

    Graphs

--- graphs.py
+++ graphs.py
@@ -5,31 +5,36 @@ Date: 02/05/2020
 """
 
 from linear_data_structures import Queue
+
+
 ## Adjacency Matrix for Fully Connected and "Small" Graph using nested list as representation
 
 ## Adjacency List for Graph using Nested Dictionary as Internal Representation
 class Vertex:
-    def __init__(self,key):
-        self.id=key
-        self.isconnectedTo={}
-        self.color=None
-        self.distance=None
-        self.pred=[]
+    def __init__(self, key):
+        self.id = key
+        self.isconnectedTo = {}
+        self.color = None
+        self.distance = None
+        self.pred = []
 
     def setPred(self, pre):
-        self.pred=pre
+        self.pred = pre
 
     def setDistance(self, n):
-        self.distance=n
+        self.distance = n
 
-    def setColor(self,flavor):
-        self.color=flavor
+    def setColor(self, flavor):
+        self.color = flavor
 
-    def addNeighbor(self, nbr,weight):
-        self.isconnectedTo[nbr]=weight
+    def addNeighbor(self, nbr, weight):
+        self.isconnectedTo[nbr] = weight
+
+    def getWeight(self, nbr):
+        return self.isconnectedTo[nbr]
 
     def isConnectedto(self):
-        return self.isconnectedTo.keys()
+        return self.isconnectedTo.key()
 
     def getPred(self):
         return self.Pred
@@ -41,7 +46,8 @@ class Vertex:
         return self.color
 
     def __str__(self):
-        return str(self.id) +  ' ' + 'is connected to'  + '' + ':' + str([i for i in self.isconnectedTo.keys()])
+        return str(self.id) + ' ' + 'is connected to' + '' + ':' + str([i for i in self.isconnectedTo.keys()])
+
 
 class Graph:
     def __init__(self):
@@ -78,41 +84,62 @@ class Graph:
     def getVertexes(self):
         return self.vertList.keys()
 
-
     def __contains__(self, item):
         return item in self.vertList
 
     def __iter__(self):
         return iter(self.vertList.values())
 
-def bfs( ):
-    start=Vertex()
-    VertQueue=Queue()
+
+def buildGraph():
+    g = Graph()
+    d = {}
+    a = ['edge', 'wedge', 'sledge', 'hedge', 'ledge']
+    for word in a:
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i + 1:]
+            print(bucket)
+            if word in d:
+                d[bucket].append([word])
+            else:
+                d[bucket] = [word]
+
+        for bucket in d.keys():
+            for word1 in d[bucket]:
+                for word2 in d[bucket]:
+                    if word1 != word2:
+                        g.addAsymetricEdge(word1, word2)
+
+
+def bfs(entry):
+    start = Graph().vertList['{}'.format(entry)]
+    VertQueue = Queue()
     start.setPred(None)
     start.setColor(None)
     start.setDistance(0)
     VertQueue.enqueue(start)
-    while VertQueue.size() > 0 :
-        currentVert=VertQueue.dequeue()
-        for nbr in currentVert.isconnectedTo():
-            if nbr.getColor()== 'white':
-                nbr.setPred(currentVert
+    while VertQueue.size() > 0:
+        currentNode = VertQueue.dequeue()
+        for nbr in start.isconnectedTo():
+            if nbr.getColor() == 'white':
                 nbr.setColor('grey')
-                nbr.setDistance(currentVert.getDistance() + 1)
+                nbr.setDistance(currentNode.getDistance() + 1)
                 VertQueue.enqueue(nbr)
-        currentVert.setColor('black')
+                currentNode.setPred(nbr)
+        currentNode.setColor('black')
 
 
 
-if __name__=='__main__':
-    a=Graph()
+
+if __name__ == '__main__':
+    a = Graph()
     a.addVertex('v1')
     a.addVertex('v2')
-    a.addEdge('v1','v2')
+    a.addEdge('v1', 'v2')
     a.getVertexes()
     a.getVert('v1')
     a['v1']
-    iter=[]
+    iter = []
     for i in a:
         iter.append(i)
     print(i)
